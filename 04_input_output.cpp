396cc736162a3fe9d467dda5655f6d34c02e3a41
Ritusmita Dutta <162014467+Ritusmita09@users.noreply.github.com>
2026-08-19 00:16:00 +0530

    operators

---
+++ 04_input_output.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int a , b;
+    cout<<"Enter your number:";cin>>a;
+    cout<<"Enter your number:";cin>>b;
+    cout<<"A="<<a <<"\n"<< "B="<<b <<"\n"<< "sum is="<<a+b;
+} 
+
+// #include<stdio.h>
+// int main(){
+//     int a,b,sum;
+//     printf("Enter Your Number:");// cout<<"Enter Your Number:";
+//     scanf("%d",&a);// cin>> a;
+//     printf("Enter Your Number:");// cout<<"Enter Your Number:";
+//     scanf("%d",&b);//cin>>b;
+//     sum =a+b;
+//     printf("%d",sum);
+// }
---
+++ 05_operators.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    double a , b;
+    cout<<"Enter a=";cin>>a;
+    cout<<"Enter b=";cin>>b;
+    cout<<"a+b="<<a+b<<"\n"<<"a-b="<<a-b<<"\n"<<"a*b="<<a*b<<"\n"<<"a/b="<<a/b;
+}
