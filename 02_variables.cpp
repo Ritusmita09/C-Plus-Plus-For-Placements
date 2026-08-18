bd809e716352f898032375cf35f74de52ab70fa9
Ritusmita Dutta <162014467+Ritusmita09@users.noreply.github.com>
2026-08-18 23:40:00 +0530

    data types

---
+++ 02_variables.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter Your Number:";cin>>n;
+    cout<<n;
+}
---
+++ 03_data_types.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;  // 3
+    double a; // 3.14
+    char s; // a ,b ,c
+    string ch; // "Ritusmita"
+    long q; // very long number > 10^8
+    long long e; // very very very long number > 10^15
+    cout << "Enter the Integer:";cin >> n;
+    cout << "Enter the Floating Point Number:";cin >> a;
+    cout << "Enter the Charecter:";cin >> s;
+    cout << "Enter the String:";cin >> ch;
+    cout << "Enter the Long Number:";cin >> q;
+    cout << "Enter the long Long Number:";cin >> e;
+    cout <<n<<"\n"<<a<<"\n"<<s<<"\n"<<ch<<"\n"<<q<<"\n"<<e<<"\n";
+}
