bfe6c0e004bbe2afb556bf7a7b59045ab27bf845
Ritusmita Dutta <162014467+Ritusmita09@users.noreply.github.com>
2026-08-19 00:58:17 +0530

    score&attendance

---
+++ 06_if_else.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter your number:";cin>>n;
+    if(n%2 == 0){
+        cout<<"NUMBER IS EVEN";
+    }
+    else{
+        cout<<"NUMBER IS ODD";
+    }
+}
---
+++ 07_else_if.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter your score:";cin>>n;
+    if(n>=85){
+        cout<<"very good! you got an A";
+    }
+    else if(n>=65){
+    cout<<"keep it up! you got a B";
+    }
+    else if(n>=45){
+        cout<<"better luck next time!you got a C";
+    }
+    else{
+        cout<<"sorry!you failed";
+    }
+}
+
+
+
+
