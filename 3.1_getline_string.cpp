ff6a0a7c5073b849fff43cec2ffa5d02c70f983c
Ritusmita Dutta <162014467+Ritusmita09@users.noreply.github.com>
2026-09-02 20:14:01 +0530

    commit

---
+++ 3.1_getline_string.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string name;
+    getline(cin,name); //it picks up till the string line breaks as you press enter
+    cout<<name;
+    return 0;
+}
